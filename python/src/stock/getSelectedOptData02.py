2e23a877586d439ac93901b3a3b1de496925285e
HaoZhou2019 <hao_zhou@yahoo.com>
2024-01-22 09:59:03 -0600

    Modified code

--- python/src/stock/getSelectedOptData02.py
+++ python/src/stock/getSelectedOptData02.py
@@ -6,12 +6,12 @@ def get_calls_and_puts(symbol: str, date: str) -> tuple:
     puts = options.get_puts(symbol, date)
 
     # Extract the desired columns
-    new_calls = calls.loc[:, ["Contract Name", "Strike", "Last Price", "Bid", "Ask"]]
-    new_puts = puts.loc[:, ["Contract Name", "Strike", "Last Price", "Bid", "Ask"]]
+    #new_calls = calls.loc[:, ["Contract Name", "Strike", "Last Price", "Bid", "Ask"]]
+    #new_puts = puts.loc[:, ["Contract Name", "Strike", "Last Price", "Bid", "Ask"]]
     
     # Extract the desired columns
-    #new_calls = calls[["Contract Name", "Strike", "Last Price", "Bid", "Ask"]]
-    #new_puts = puts[["Contract Name", "Strike", "Last Price", "Bid", "Ask"]]
+    new_calls = calls[["Contract Name", "Strike", "Last Price", "Bid", "Ask"]]
+    new_puts = puts[["Contract Name", "Strike", "Last Price", "Bid", "Ask"]]
 
     return new_calls, new_puts
 
