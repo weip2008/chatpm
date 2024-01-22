1313ef1258dbcf936c6c0d2481ea194e207ed86e
HaoZhou2019 <hao_zhou@yahoo.com>
2024-01-22 11:28:57 -0600

    Updated code, added calculated column to filted call/put DataFrame object

--- python/src/stock/getSelectedOptData02.py
+++ python/src/stock/getSelectedOptData02.py
@@ -1,5 +1,11 @@
+import warnings
+
 from yahoo_fin import options
+from yahoo_fin.stock_info import get_live_price
+#import yfinance as yf
 import pandas as pd
+
+warnings.filterwarnings('ignore')
 
 def get_calls_and_puts(symbol: str, date: str) -> tuple:
     calls = options.get_calls(symbol, date)
@@ -15,6 +21,14 @@ def get_calls_and_puts(symbol: str, date: str) -> tuple:
 
     return new_calls, new_puts
 
+
+def add_sign_column(df, currentPrice):
+    # create a new column "Sign" and calculate its value
+    df["Sign"] = (df["Bid"] + df["Ask"]) / 2.0 / currentPrice
+    # return the updated DataFrame
+    return df
+
+
 symbol = "LULU"
 date = "01/26/2024"
 
@@ -23,8 +37,26 @@ calls, puts = get_calls_and_puts(symbol, date)
 calls.to_csv("python/data/LULU_calls_selected.csv")
 puts.to_csv("python/data/LULU_puts_selected.csv")
 
+print("\n========================= CALLS ===============================")
 print(calls)
-print("\n========================================================\n")
+print("\n========================== PUTS ===============================")
 print(puts)
 
+print("\n========================================================\n")
+current_price = get_live_price(symbol)
+print(f"The current price of {symbol} is ${current_price:.2f}")
 
+# data = yf.download(symbol, period="1d")
+# current_price = data["Close"][0]
+# print(f"The current price of {symbol} is ${current_price:.2f}")
+
+calculated_calls = add_sign_column(calls, current_price)
+calculated_puts = add_sign_column(puts, current_price)
+#sorted_calculated_calls = calculated_calls.sort_values(by="Sign")
+print("\n========================= Calculated CALLS ===============================")
+print(calculated_calls)
+print("\n========================= Calculated PUTS ===============================")
+print(calculated_puts)
+
+calls.to_csv("python/data/LULU_calls_calculated.csv")
+puts.to_csv("python/data/LULU_puts_calculated.csv")
