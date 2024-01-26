5c64caf76e76e37d2768f95e569967a355edd094
HaoZhou2019 <hao_zhou@yahoo.com>
2024-01-26 12:28:32 -0600

    Updated code, added symbole list

--- python/doc/stock.md
+++
@@ -1,6 +0,0 @@
-<h1>Stock Study Notes</h1>
-
-```
-pip install pandas_datareader
-(.venv) C:\Users\wangq\workspace\students\hov\python>pip install yfinance
-```
--- python/src/stock/getSelectedOptData02.py
+++ python/src/stock/getSelectedOptData02.py
@@ -4,16 +4,20 @@ from yahoo_fin import options
 from yahoo_fin.stock_info import get_live_price
 #import yfinance as yf
 import pandas as pd
+import numpy as np
 
 warnings.filterwarnings('ignore')
+
+symbolList = ["BUD", "PBR", "TSM", "BABA", "NVO", "PDD", "SHEL", "SHOP", "SONY", "HSBC", "AZN", "ASML"]
+#symbolList = ["ASML","BUD"]
 
 def get_calls_and_puts(symbol: str, date: str) -> tuple:
     calls = options.get_calls(symbol, date)
     puts = options.get_puts(symbol, date)
 
-    # Extract the desired columns
-    #new_calls = calls.loc[:, ["Contract Name", "Strike", "Last Price", "Bid", "Ask"]]
-    #new_puts = puts.loc[:, ["Contract Name", "Strike", "Last Price", "Bid", "Ask"]]
+    # Replace '-' with 0
+    calls = calls.replace("-", 0)
+    puts = puts.replace("-", 0)
     
     # Extract the desired columns
     new_calls = calls[["Contract Name", "Strike", "Last Price", "Bid", "Ask", "Volume"]]
@@ -21,17 +25,10 @@ def get_calls_and_puts(symbol: str, date: str) -> tuple:
 
     return new_calls, new_puts
 
-
-""" def add_sign_column(df, currentPrice):
-    # create a new column "Sign" and calculate its value
-    df["Sign"] = (df["Bid"] + df["Ask"]) / 2.0 / currentPrice
-    # return the updated DataFrame
-    return df """
-
-
 def add_sign_column(df):
     # create a new column "Sign" and calculate its value
-    df["Sign"] = (df["Bid"] + df["Ask"]) / 2.0 / df["Strike"]
+    # df["Sign"] = (df["Bid"] + df["Ask"]) / 2.0 / df["Strike"]
+    df["Sign"] = (df["Bid"].astype(float) + df["Ask"].astype(float)) / 2.0 / df["Strike"].astype(float)
     # return the updated DataFrame
     return df
 
@@ -72,57 +69,63 @@ def filter_rows_by_name(df, given_price):
     
     return result
 
-symbol = "LULU"
+
 contractDate = "01/26/2024"
-path = "python/data/"
+#path = "python/data/"
+path = "d:/chatpm/python/data/"
 today = date.today()
 print(f"Today's date is {today.strftime('%m-%d-%Y')}")
 now = datetime.now()
 currentDatetime = now.strftime("%m%d%Y-%H%M")
 print(f"Current date time is " + currentDatetime)
 
-# Get calls and puts for given stock at contract date
-calls, puts = get_calls_and_puts(symbol, contractDate)
+for symbol in symbolList:
+    #symbol = "LULU"
+    print("\n\nsymbol:"+symbol)
+    
+    # Get calls and puts for given stock at contract date
+    calls, puts = get_calls_and_puts(symbol, contractDate)
 
-fileName = path+symbol+"_call_selected_"+today.strftime('%m-%d-%Y')+".csv"
-print("File name:"+fileName)
-calls.to_csv(fileName)
-fileName = path+symbol+"_put_selected_"+today.strftime('%m-%d-%Y')+".csv"
-puts.to_csv(fileName)
+    """ fileName = path+symbol+"_call_selected_"+today.strftime('%m-%d-%Y')+".csv"
+    print("File name:"+fileName)
+    calls.to_csv(fileName)
+    fileName = path+symbol+"_put_selected_"+today.strftime('%m-%d-%Y')+".csv"
+    puts.to_csv(fileName)
 
-print("\n========================= CALLS ===============================")
-print(calls)
-print("\n========================== PUTS ===============================")
-print(puts)
+    print("\n========================= CALLS ===============================")
+    print(calls)
+    print("\n========================== PUTS ===============================")
+    print(puts)
+"""
+ 
+    calculated_calls = add_sign_column(calls)
+    calculated_puts = add_sign_column(puts)
 
-calculated_calls = add_sign_column(calls)
-calculated_puts = add_sign_column(puts)
+    print("\n========================= Calculated CALLS ===============================")
+    print(calculated_calls)
+    print("\n========================= Calculated PUTS ===============================")
+    print(calculated_puts)
 
-print("\n========================= Calculated CALLS ===============================")
-print(calculated_calls)
-print("\n========================= Calculated PUTS ===============================")
-print(calculated_puts)
+    fileName = path+symbol+"_calls_calculated_"+today.strftime('%m-%d-%Y')+".csv"
+    calculated_calls.to_csv(fileName)
+    fileName = path+symbol+"_puts_calculated_"+today.strftime('%m-%d-%Y')+".csv"
+    calculated_puts.to_csv(fileName)
 
-fileName = path+symbol+"_calls_calculated_"+today.strftime('%m-%d-%Y')+".csv"
-calculated_calls.to_csv(fileName)
-fileName = path+symbol+"_puts_calculated_"+today.strftime('%m-%d-%Y')+".csv"
-calculated_puts.to_csv(fileName)
+    print("\n========================================================\n")
+    current_price = get_live_price(symbol)
+    print(f"The current price of {symbol} is ${current_price:.2f}")
 
-print("\n========================================================\n")
-current_price = get_live_price(symbol)
-print(f"The current price of {symbol} is ${current_price:.2f}")
+    filtered_calls = filter_rows_by_price(calculated_calls, current_price)
+    filtered_puts = filter_rows_by_price(calculated_puts, current_price)
 
-filtered_calls = filter_rows_by_price(calculated_calls, current_price)
-filtered_puts = filter_rows_by_price(calculated_puts, current_price)
+    print(f"\n======================= Filtered CALLS for ${current_price:.2f}===========================" )
+    print(filtered_calls)
+    print(f"\n======================= Filtered PUTS for ${current_price:.2f}============================")
+    print(filtered_puts)
 
-print(f"\n======================= Filtered CALLS for ${current_price}===========================" )
-print(filtered_calls)
-print(f"\n======================= Filtered PUTS for ${current_price}============================")
-print(filtered_puts)
-
-#fileName = path+symbol+"_calls_filtered_"+today.strftime('%m-%d-%Y')+".csv"
-fileName = path+symbol+"_calls_filtered_"+currentDatetime+".csv"
-filtered_calls.to_csv(fileName)
-#fileName = path+symbol+"_puts_filtered_"+today.strftime('%m-%d-%Y')+".csv"
-fileName = path+symbol+"_puts_filtered_"+currentDatetime+".csv"
-filtered_puts.to_csv(fileName)
+    #fileName = path+symbol+"_calls_filtered_"+today.strftime('%m-%d-%Y')+".csv"
+    fileName = path+symbol+"_calls_filtered_"+currentDatetime+".csv"
+    filtered_calls.to_csv(fileName)
+    #fileName = path+symbol+"_puts_filtered_"+today.strftime('%m-%d-%Y')+".csv"
+    fileName = path+symbol+"_puts_filtered_"+currentDatetime+".csv"
+    filtered_puts.to_csv(fileName)
